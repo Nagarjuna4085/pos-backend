05b0dee32bed102def84309ab7e86ad5ce51ba45
Nagarjuna4085 <nagarjuna.sanem@gmail.com>
2026-04-25 01:22:15 -0400

    project template,db and hybernate setup

---
+++ pos-template/src/main/java/com/thenocturn/pos/PosTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.thenocturn.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTemplateApplication.class, args);
+	}
+
+}
---
+++ pos-template/src/test/java/com/thenocturn/pos/PosTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenocturn.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
